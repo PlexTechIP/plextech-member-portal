e3ead3454e157b46e504aad5b444753eda0a48ba
Shamith Pasula <shamith09@gmail.com>
2023-01-22 17:08:45 -0800

    profile no work

--- src/app/pages/ProfilePage/index.tsx
+++ src/app/pages/ProfilePage/index.tsx
@@ -9,7 +9,12 @@ import styled from 'styled-components/macro';
 interface Props {}
 
 export function ProfilePage(props: Props) {
-  return <Div></Div>;
+  return (
+    <Div>
+      Profile Page under construction, click menu on top left and go to
+      Reimbursements page for more
+    </Div>
+  );
 }
 
 const Div = styled.div``;
