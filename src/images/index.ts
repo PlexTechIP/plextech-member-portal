c86fe91abc394deb98a38d2be5cb650f7dcc2367
Shamith Pasula <shamith09@gmail.com>
2023-06-27 13:09:53 -0700

    fixed images

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,7 +1,17 @@
-export const PCold = './PCold.png';
-export const PError = './PError.png';
-export const PRegular = './PRegular.png';
-export const PlexTechLogo = './PlexTechLogo.png';
-export const shapesBackgroundDark = './shapes-background-dark.png';
-export const shapesBackground = './shapes-background.png';
-export const errorImg = './error.png';
+import PCold from './PCold.png';
+import PError from './PError.png';
+import PRegular from './PRegular.png';
+import PlexTechLogo from './PlexTechLogo.png';
+import shapesBackgroundDark from './shapes-background-dark.png';
+import shapesBackground from './shapes-background.png';
+import errorImg from './error.png';
+
+export {
+  PCold,
+  PError,
+  PRegular,
+  PlexTechLogo,
+  shapesBackgroundDark,
+  shapesBackground,
+  errorImg,
+};
