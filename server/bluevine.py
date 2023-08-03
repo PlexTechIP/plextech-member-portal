946b06e05b4ebd8063665333ee404388c31e12ef
Shamith Pasula <shamith09@gmail.com>
2023-08-03 00:24:37 -0700

    update ACH time

--- server/bluevine.py
+++ server/bluevine.py
@@ -225,7 +225,7 @@ def after_login(
             email,
             "Reimbursement Request Approved",
             f"Hi {fullName},",
-            f'Your reimbursement request of ${amount} for "{description}" has been approved. The ACH transfer may take up to 2 business days to complete. If you do not receive the money by then, please contact info@plextech.berkeley.edu or a PlexTech Executive Board member.',
+            f'Your reimbursement request of ${amount} for "{description}" has been approved. The ACH transfer may take up to 5 business days to complete. If you do not receive the money by then, please contact info@plextech.berkeley.edu or a PlexTech Executive Board member.',
         )
 
     return {}, 200
